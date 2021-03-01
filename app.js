50cb1bd7364d1feb25a5a7f09738ee513082a409
Terrance Lee <takhol@andrew.cmu.edu>
2021-03-02 00:44:06 +0900

    done

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-
 const colors = document.getElementsByClassName("jsColor");
-
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
 canvas.width = 600;
 canvas.height = 600;
 
-ctx.strokeStyle = "black";
+const DEFAULT_COLOR = "black";
+makeDefaultBackground();
+
+ctx.strokeStyle = DEFAULT_COLOR;
+ctx.fillStyle = DEFAULT_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
+
+function makeDefaultBackground(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 function stopPainting(){
     painting = false;
@@ -42,20 +52,35 @@ function onMouseUp(event){
 }
 
 function onMouseLeave(event){
-    //stopPainting();
+    stopPainting();
 }
+
+function handleCanvasClick(event){
+    if(filling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        alert(`The canvas will be filled with ${ctx.fillStyle}`);
+        mode.innerText = "Fill";
+    }
+    filling = false;
+}
+
+function handleRightClick(event){
+    event.preventDefault();
+};
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", onMouseDown);
     canvas.addEventListener("mouseup", onMouseUp);
     canvas.addEventListener("mouseleave", onMouseLeave);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleRightClick);
 }
 
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
-    console.log(color);
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 Array.from(colors).forEach(color =>
@@ -67,4 +92,29 @@ function handleBrushSizeClick(event){
 }
 if(range){
     range.addEventListener("input", handleBrushSizeClick);
+}
+
+function handleModeClick(event){
+    if(filling){
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Filling";
+    }
+}
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+function handleSave(event){
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "New Painting";
+    link.click();
+}
+
+if(save){
+    save.addEventListener("click", handleSave);
 }
